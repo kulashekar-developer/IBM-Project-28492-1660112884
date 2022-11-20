81932524f7a0430a40b0aaaba1b983847e1faae2
Developer Kulashekar <85106994+kulashekar-developer@users.noreply.github.com>
2022-11-20 11:11:09 +0530

    Add files via upload

---
+++ Assignments/Team Leader/ASSIGNMENT 4/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
